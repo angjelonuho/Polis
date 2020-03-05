10ab07b903b49491e7b4de9a1bdae2ace2bdabae
Angjelo <33224333+angjelonuho@users.noreply.github.com>
2020-03-05 14:49:14 +0100

    Google map added

--- src/App.js
+++ src/App.js
@@ -2,28 +2,15 @@ import React from 'react';
 import './css/App.css';
 import DayNight from './components/dayNightComponent';
 import GenerateBuildings from './components/buildings';
-import GoogleMapComponentWithMarker from './components/GoogleMapWithMarker'
+import GoogleMapComp from './components/GoogleMapWithMarker'
 
-
-// Some default styles
-const styles = {
-    width: '100%',
-    height: '536px'
-  }
 
 function App() {
     return (
         <div className="App container-fluid">
             <DayNight/>
             <GenerateBuildings/>
-            <div style={styles}>
-        <GoogleMapComponentWithMarker
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyD8k5qUSvlHzgITZ4o1icvEgpMromP4s2c"
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `100%` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
-      </div>
+            <GoogleMapComp/>
             
         </div>
         
