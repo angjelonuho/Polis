893010c1292d457568caacfad30aa4710b4164b0
Angjelo <33224333+angjelonuho@users.noreply.github.com>
2020-04-06 18:14:15 +0200

    Updates

---
+++ precache-manifest.158bc33c40343bacb3e3335057d34908.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df45ec08fff3e9adae2c661e82b51d7a",
+    "url": "/Polis/index.html"
+  },
+  {
+    "revision": "41c266a692df0d00bf83",
+    "url": "/Polis/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "77841c4446e1bd1f0962",
+    "url": "/Polis/static/css/main.bcd3361b.chunk.css"
+  },
+  {
+    "revision": "41c266a692df0d00bf83",
+    "url": "/Polis/static/js/2.497dc35d.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/Polis/static/js/2.497dc35d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77841c4446e1bd1f0962",
+    "url": "/Polis/static/js/main.638d4807.chunk.js"
+  },
+  {
+    "revision": "c4d280610ee059393662",
+    "url": "/Polis/static/js/runtime-main.de395dea.js"
+  },
+  {
+    "revision": "47cfd45b3574f07b1ac56116c60c9037",
+    "url": "/Polis/static/media/droplet.47cfd45b.svg"
+  },
+  {
+    "revision": "8e1520570a238ac123450b94a4cc2c1c",
+    "url": "/Polis/static/media/sunrise.8e152057.svg"
+  },
+  {
+    "revision": "cdc8210eb1d696962d411413bb89a603",
+    "url": "/Polis/static/media/sunset.cdc8210e.svg"
+  },
+  {
+    "revision": "8255af024d60203d4ec7fbf9a6240ff3",
+    "url": "/Polis/static/media/wind.8255af02.svg"
+  }
+]);
