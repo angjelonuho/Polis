c88bfa7131be99ab4b4b2ef3c2bc2b61cecf7638
Angjelo <33224333+angjelonuho@users.noreply.github.com>
2020-04-10 13:47:37 +0200

    s

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,10 +2,13 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 
 export default function(app){
 <<<<<<< HEAD
+<<<<<<< HEAD
     app.use(createProxyMiddleware ("", {target: "https://maps.googleapis.com", changeOrigin: true}));
     app.use(createProxyMiddleware ("",{ target: "http://api.openweathermap.org", changeOrigin: true}));
     app.use(createProxyMiddleware ("",{ target: "https://api.exchangeratesapi.io", changeOrigin: true}));
 =======
+=======
+>>>>>>> parent of 3380a1e... f
     app.use(createProxyMiddleware ("/maps", {target: "https://maps.googleapis.com", changeOrigin: true}));
     app.use(createProxyMiddleware ("/api.openweathermap.org",{ target: "http://", changeOrigin: true}));
     app.use(createProxyMiddleware ("/latest",{ target: "https://api.exchangeratesapi.io/", changeOrigin: true}));
