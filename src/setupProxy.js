318e2bbcd27df24ce70f821d9745967e35dac5e3
Angjelo <33224333+angjelonuho@users.noreply.github.com>
2020-04-10 18:01:31 +0200

    didi iiit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,8 @@
 const {createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app){
-    app.use(createProxyMiddleware ("/maps", {target: "//maps.googleapis.com", changeOrigin: true}));
-    app.use(createProxyMiddleware ("/api.openweathermap.org",{ target: "https://", changeOrigin: true}));
-    app.use(createProxyMiddleware ("/latest",{ target: "https://api.exchangeratesapi.io", changeOrigin: true}));
+    app.use(createProxyMiddleware ("/maps", {target: "https://angjelonuho.github.io", changeOrigin: true}));
+    app.use(createProxyMiddleware ("/api.openweathermap.org",{ target: "http://", changeOrigin: true}));
+    app.use(createProxyMiddleware ("/latest",{ target: "https://api.exchangeratesapi.io/", changeOrigin: true}));
     
 }; 
