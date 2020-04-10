e648080c4d93efb73ae1cf78399e811185c74d26
Angjelo <33224333+angjelonuho@users.noreply.github.com>
2020-04-10 14:02:21 +0200

    fix cors

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,8 @@
 const {createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app){
-    app.use(createProxyMiddleware ("/maps", {target: "https://maps.googleapis.com", changeOrigin: true}));
+    app.use(createProxyMiddleware ("/maps.googleapis.com", {target: "https://", changeOrigin: true}));
     app.use(createProxyMiddleware ("/api.openweathermap.org",{ target: "http://", changeOrigin: true}));
-    app.use(createProxyMiddleware ("/latest",{ target: "https://api.exchangeratesapi.io/", changeOrigin: true}));
+    app.use(createProxyMiddleware ("/api.exchangeratesapi.io/",{ target: "https://", changeOrigin: true}));
     
 }; 
