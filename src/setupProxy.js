cec31019c88d65e2e74a58fdd89e2198720fe332
Angjelo <33224333+angjelonuho@users.noreply.github.com>
2020-04-13 17:59:00 +0200

    da

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,6 @@ const {createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {
     app.use(
-      '/maps/api/place',
       createProxyMiddleware({
         target: 'https://maps.googleapis.com',
         changeOrigin: true,
